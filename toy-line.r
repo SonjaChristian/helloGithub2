28134a2482ddd07943551f91eaab2982346bda41
Sonja <sonja7941@gmail.com>
2015-06-03 14:58:05 -0700

    change

--- toy-line.r
+++ toy-line.r
@@ -8,3 +8,9 @@ y <- a + b * x + rnorm(40, sd = sqrt(sig_sq))
 write(avg_x, "avg_x.txt")
 plot(x, y)
 abline(a, b, col = "purple")
+<<<<<<< HEAD
+dev.print(pdf, "toy_line_plot.pdf")
+changedFiles()
+=======
+dev.print(pdf, "toy_line_plot.pdf")
+>>>>>>> 2aeab39cf94057f2b93a429c333ed01b1b9ab3e9
